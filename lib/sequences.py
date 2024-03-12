0eef282dda3139cb969b0a52095becc06ca370bf
Samuel Chu <samueljchu0126@gmail.com>
2024-03-12 12:47:44 -0400

    sequences assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]
+    if length <= 0:
+        sequence = []
+    elif length == 1:
+        sequence = [0]
+    elif length > 2:
+        for i in range(2, length):
+            sequence.append(sequence[i-1] + sequence[i-2])
+    print(sequence)
+
